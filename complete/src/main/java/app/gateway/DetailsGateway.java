b471fee420466c30043296557ab134f4cdaee974
Lar Szuwalski <lars@Lars-MacBook-Pro.local>
2016-08-25 01:29:42 +0200

    v5

--- complete/src/main/java/app/gateway/DetailsGateway.java
+++ complete/src/main/java/app/gateway/DetailsGateway.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import rx.Observable;
 
+import java.util.Collections;
+import java.util.concurrent.TimeUnit;
+
 @RestController
 public class DetailsGateway {
     private final Logger log = LoggerFactory.getLogger(DetailsGateway.class);
@@ -25,11 +28,20 @@ public class DetailsGateway {
 
                 rest.get("part?partId=" + partId, JsonNode.class),
 
-                (stock, description) -> {
+                Observable.merge(
+                        partReviews(), partTipsAndTricks(), partOnTwitter()
+                )
+                        .take(200, TimeUnit.MILLISECONDS)
+                        .buffer(10)
+                        .onErrorResumeNext(Observable.empty())
+                        .firstOrDefault(Collections.emptyList()),
+
+                (stock, description, reviews) -> {
                     ObjectNode combined = JsonNodeFactory.instance.objectNode();
 
                     combined.replace("stock", stock);
                     combined.replace("part", description);
+                    combined.putArray("items").addAll(reviews);
 
                     combined.put("available", stock.get("inStock").asInt() > 0);
                     combined.put("price", String.format("%.1f0", stock.get("unitPrice").asDouble()));
@@ -37,5 +49,27 @@ public class DetailsGateway {
                     return combined;
                 }
         ).toBlocking().first();
+    }
+
+    public Observable<JsonNode> partReviews() {
+        return Observable.interval(5, TimeUnit.MILLISECONDS)
+                .map(i -> format("partReviews " + i, "review, review, review"));
+    }
+
+    public Observable<JsonNode> partTipsAndTricks() {
+        return Observable.interval(5, TimeUnit.MILLISECONDS)
+                .map(i -> format("partTipsAndTricks " + i, "tip, trick, tip, trick"));
+    }
+
+    public Observable<JsonNode> partOnTwitter() {
+        return Observable.interval(5, TimeUnit.MILLISECONDS)
+                .map(i -> format("twitter " + i, "twit, twit, twit.."));
+    }
+
+    private static JsonNode format(String header, String details) {
+        ObjectNode txt = JsonNodeFactory.instance.objectNode();
+        txt.put("header", header);
+        txt.put("details", details);
+        return txt;
     }
 }
